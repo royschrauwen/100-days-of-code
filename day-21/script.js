c8189e8feaf502c00624d555c3096e08689fe647
Roy Schrauwen <roy@royschrauwen.nl>
2022-02-19 09:38:13 +0100

    Basic Fetch setup

--- day-21/script.js
+++ day-21/script.js
@@ -1,1 +1,32 @@
-console.log("script.js connected");
+// https://api.adviceslip.com/
+/*
+Random advice
+Note: Advice is cached for 2 seconds. Any repeat-request within 2 seconds will return the same piece of advice.
+
+HTTP Method	GET
+URL	https://api.adviceslip.com/advice
+Description	Returns a random advice slip as a slip object.
+Parameters	
+callback
+string To define your own callback function name and return the JSON in a function wrapper (as JSONP), add the parameter callback with your desired name as the value.
+*/
+
+const adviceNumber = document.querySelector("#advice-number");
+const adviceText = document.querySelector("#advice-text");
+const adviceButton = document.querySelector("#advice-button");
+
+const url = "https://api.adviceslip.com/advice";
+
+adviceButton.addEventListener("click", showNewAdvice);
+
+function showNewAdvice() {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      adviceNumber.textContent = data.slip.id;
+      adviceText.textContent = data.slip.advice;
+    });
+}
